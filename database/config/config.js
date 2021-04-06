bc74a5abd381ff6f221f29bd9345264b258ee023
Viktor Peredera <viktor.peredera@abz.agency>
2021-04-06 21:13:39 +0300

    fix: process env var

--- database/config/config.js
+++ database/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { DB_URL } = process.env.DATABASE_URL;
 
 module.exports = {
-    url: 'postgres://syxkusnzeijxzx:e36e2656851a3e3f8a81a2b51fed04a5c788aaeb54d9c7c81519820e9742c242@ec2-52-44-31-100.compute-1.amazonaws.com:5432/d2mglhlrgao50h',
+    url: DB_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
