284dd62df7a9363a8f4b1012a9141b86f74ef918
Viktor Peredera <viktor.peredera@abz.agency>
2021-04-18 21:48:44 +0300

    fix: module fix

--- redis.js
+++ redis.js
@@ -1,24 +1,26 @@
-import * as redis from 'redis';
+const redis = require('redis');
 const client = redis.createClient(process.env.REDIS_URL);
 
-export const setToRedis = (key, value) => {
-  return new Promise((resolve, reject) => {
-    client.set(key, value, (err, reply) => {
-      if(err) {
-        reject(err);
-      }
-      resolve();
+module.exports = {
+  setToRedis: (key, value) => {
+    return new Promise((resolve, reject) => {
+      client.set(key, value, (err, reply) => {
+        if(err) {
+          reject(err);
+        }
+        resolve();
+      })
     })
-  })
+  },
+  getFromRedis: (key) => {
+    return new Promise((resolve, reject) => {
+      client.get(key, (err, reply) => {
+        if(err) {
+          reject();
+        }
+        resolve(reply);
+      })
+    })
+  }
 }
 
-export const getFromRedis = (key) => {
-  return new Promise((resolve, reject) => {
-    client.get(key, (err, reply) => {
-      if(err) {
-        reject();
-      }
-      resolve(reply);
-    })
-  })
-}
