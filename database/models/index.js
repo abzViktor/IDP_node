296446332af3fd56094bbea64d8a359528f643e4
Viktor Peredera <viktor.peredera@abz.agency>
2021-05-16 23:08:14 +0300

    fix: add logs

--- database/models/index.js
+++ database/models/index.js
@@ -6,6 +6,8 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
+console.log(config);
+console.log(env);
 const db = {};
 
 let sequelize;
