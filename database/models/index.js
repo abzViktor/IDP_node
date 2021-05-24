82fc88d9774cae905b602722ecbc97026eab6a79
Viktor Peredera <viktor.peredera@abz.agency>
2021-05-24 23:28:57 +0300

    fix: fixed12

--- database/models/index.js
+++ database/models/index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
+const {Sequelize} = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
@@ -12,7 +12,7 @@ let sequelize;
 console.log(config);
 if (config.url) {
   console.log("!1");
-  sequelize = new Sequelize(process.env[config.url], config);
+  sequelize = new Sequelize(config.url, config);
   console.log(sequelize);
 } else {
   console.log("!2");
